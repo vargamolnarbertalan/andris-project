38a9f8f794eb1b11a73e9f10b4251378522afe05
vargamolnarbertalan <vargamolnarb@gmail.com>
2025-07-29 21:41:36 +0200

    #5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./Splash.css"; // We'll style it here
 import BaseTemplate from './BaseTemplate'; // Update the path as per your project structure
 
-function App() {
-  return (
-    <Router>
-      <Routes>
+const SplashScreen = ({ onFinish }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onFinish();
+    }, 2000); // 2 seconds
 
-        {/* Catch-all route for all other paths */}
-        <Route path="*" element={<MainApp />} />
-      </Routes>
-    </Router>
+    return () => clearTimeout(timer);
+  }, [onFinish]);
+
+  return (
+    <div className="splash-screen">
+      <img src="/img/splash-art.png" alt="Splash" className="splash-image" />
+    </div>
+  );
+};
+
+function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  return (
+    <>
+      {showSplash ? (
+        <SplashScreen onFinish={() => setShowSplash(false)} />
+      ) : (
+          <Router>
+            <Routes>
+
+              {/* Catch-all route for all other paths */}
+              <Route path="*" element={<MainApp />} />
+            </Routes>
+          </Router>
+      )}
+    </>
   );
 }
 
