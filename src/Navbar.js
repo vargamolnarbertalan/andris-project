c0b0d5a2806a11254eb9be559327538d4de02cb2
vargamolnarbertalan <vargamolnarb@gmail.com>
2025-09-09 17:02:48 +0200

    #17 v1.4

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,6 +8,15 @@ const Navbar = () => {
     const { t } = useTranslation();
     const [isVisible, setIsVisible] = useState(false);
 
+    // Function to close navbar on mobile when link is clicked
+    const closeNavbar = () => {
+        const navbarCollapse = document.getElementById('navbarNav');
+        if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+            // Simply remove the 'show' class to close the navbar
+            navbarCollapse.classList.remove('show');
+        }
+    };
+
     window.onscroll = function () {
         const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
         const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -22,39 +31,45 @@ const Navbar = () => {
                 <a className="mynavbar-brand navbar-brand" href="#">
                     <img id="navLogo" className="navImage" src="/img/mainlogo.jpg" alt='logo'></img>
                 </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                {/* Right side container for hamburger menu and language switcher (mobile only) */}
+                <div className="d-flex flex-row align-items-center gap-2 d-lg-none">
+                    {/* Language switcher - visible on mobile only */}
+                    <LangSwitcher />
+                    
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
 
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                            <a className="mynavbar-text nav-link" href="#aboutSection">{t('about')}</a>
+                            <a className="mynavbar-text nav-link" href="#aboutSection" onClick={closeNavbar}>{t('about')}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="mynavbar-text nav-link" href="#missionSection">{t('mission')}</a>
+                            <a className="mynavbar-text nav-link" href="#missionSection" onClick={closeNavbar}>{t('mission')}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="mynavbar-text nav-link" href="#mediaSection">{t('media')}</a>
+                            <a className="mynavbar-text nav-link" href="#mediaSection" onClick={closeNavbar}>{t('media')}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="mynavbar-text nav-link" href="#servicesSection">{t('services')}</a>
+                            <a className="mynavbar-text nav-link" href="#servicesSection" onClick={closeNavbar}>{t('services')}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="mynavbar-text nav-link" href="#connectSection">{t('connect')}</a>
+                            <a className="mynavbar-text nav-link" href="#connectSection" onClick={closeNavbar}>{t('connect')}</a>
                         </li>
                     </ul>
 
-                    {/* Insert LangSwitcher here, aligned right */}
-                    <div className={'d-flex align-items-left'}>
+                    {/* Language switcher - visible on desktop only, positioned on far right */}
+                    <div className="d-none d-lg-flex align-items-center">
                         <LangSwitcher />
                     </div>
                 </div>
